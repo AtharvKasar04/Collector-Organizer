50c04c7d66455bb8389c9240931753e203fec908
AtharvKasar <atharvk752@gmail.com>
2025-01-14 21:55:43 +0530

    minor fixes

--- server/app.js
+++ server/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const userModel = require("./models/UserModel");
 const cookieParser = require("cookie-parser");
 const userRouter = require("./routes/userRouter");
 
