74723fddeceea5f263fd5f184f8638a104af4097
AtharvKasar <atharvk752@gmail.com>
2025-01-15 15:26:27 +0530

    deleteItem route functionality

--- server/controllers/itemController.js
+++ server/controllers/itemController.js
@@ -19,3 +19,18 @@ module.exports.createItem = async (req, res) => {
         res.status(500).json({ message: "Error creating Item", error: error.message });
     }
 };
+
+module.exports.deleteItem = async (req, res) => {
+    let { id } = req.params;
+    let userId = req.user.id;
+
+    try {
+        const item = await collections.findOneAndDelete({ _id: id, createdBy: userId });
+        if (!item) {
+            return res.status(404).json({ message: "Item not found or unauthorized" });
+        }
+        res.status(200).json({ message: "Item deleted successfully" });
+    } catch (error) {
+        res.status(500).json({ message: "Error deleting item", error: error.message });
+    }
+}
--- server/routes/itemRouter.js
+++ server/routes/itemRouter.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const authMiddleware = require("../middlewares/authMiddleware");
 const router = express.Router();
-const { createItem } = require("../controllers/itemController");
+const { createItem, deleteItem } = require("../controllers/itemController");
 
 router.post("/create-item", authMiddleware, createItem);
+router.delete("/delete-item/:id", authMiddleware, deleteItem);
 
 module.exports = router;
