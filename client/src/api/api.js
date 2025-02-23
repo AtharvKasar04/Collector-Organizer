dad5b66c54569bbb7788eb3cf120235d64112c9e
AtharvKasar <atharvk752@gmail.com>
2025-02-23 14:39:49 +0530

    fixing CORS

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://collector-organizer.onrender.com',
+    baseURL: 'https://coll-1vctsxqmq-atharvkasars-projects.vercel.app',
     withCredentials: true
 });
 
