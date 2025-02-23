c26876ae6c23b46006144e72bbfe67a132bc5509
AtharvKasar <atharvk752@gmail.com>
2025-02-23 12:27:52 +0530

    fixing CORS

--- server/app.js
+++ server/app.js
@@ -14,6 +14,7 @@ app.use(cors({
     origin: 'https://collector-organizer.vercel.app',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: 'Content-Type,Authorization',
 }));
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
