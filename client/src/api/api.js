86a8049d65e482cc4b04296f659c70285330419a
AtharvKasar <atharvk752@gmail.com>
2025-02-23 12:24:15 +0530

    fixing CORS

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -5,12 +5,6 @@ const BASE_URL = 'https://collector-organizer.onrender.com'
 const api = axios.create({
     baseURL: BASE_URL,
     withCredentials: true,
-    headers: {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-        'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-    },
 });
 
 export default api;
